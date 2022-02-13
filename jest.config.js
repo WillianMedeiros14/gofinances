124dea3010b8fee326047d2a08c455e626d4827d
Willian Medeiros <willianmedeiroslm@gmail.com>
2022-02-13 15:41:41 -0300

    test docs: test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,18 @@ module.exports =  {
         "/android",
         "/ios"
     ],
+
     setupFilesAfterEnv: [
       "@testing-library/jest-native/extend-expect",
       "jest-styled-components"
+    ],
+
+    collectCoverage: true,
+    collectCoverageFrom: [
+      "src/**/*.tsx",
+      "!src/**/*.spec.tsx"
+    ],
+    coverageReporters: [
+      "lcov"
     ]
 };
