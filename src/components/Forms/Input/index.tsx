8b1e55af1aec8f59536ec5ee6379c689b64aa8a4
Willian Medeiros <willianmedeiroslm@gmail.com>
2022-02-12 22:29:18 -0300

    test: test component input

--- src/components/Forms/Input/index.tsx
+++ src/components/Forms/Input/index.tsx
@@ -5,10 +5,12 @@ import {
     Container
 } from './styles';
 
-type Props = TextInputProps;
+interface Props extends TextInputProps {
+    active?: boolean;
+};
 
-export function Input({...rest} : Props){
+export function Input({ active = false, ...rest} : Props){
     return(
-        <Container {...rest} />
+        <Container active={active} {...rest} />
     );
 }
