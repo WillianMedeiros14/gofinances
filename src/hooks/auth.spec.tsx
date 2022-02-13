72f4669df255fab9bcfca148f1abf8f20a03a20c
Willian Medeiros <willianmedeiroslm@gmail.com>
2022-02-13 09:45:34 -0300

    test: Custom Mocks

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,25 +1,22 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-
+import { mocked } from 'ts-jest/utils';
+import { logInAsync } from'expo-google-app-auth';
 import { AuthProvider, useAuth } from './auth';
 
-jest.mock('expo-google-app-auth', () => {
-    return{
-        logInAsync: () => {
-            return{
-                type: 'success',
-                user: {
-                    id: 'any_email',
-                    email: 'medeiros@gmail.com',
-                    name: 'Medeiros',
-                    photo: 'anya_photo.png'
-                }
-            }
-        }
-    }
-});
+jest.mock('expo-google-app-auth');
 
 describe('Auth Hook', () => {
     it('should be able to sign in with Goolge account existing', async () => {
+        const googleMocked = mocked(logInAsync as any);
+        googleMocked.mockReturnValue({
+            type: 'success',
+            user: {
+                id: 'any_id',
+                email: 'medeiros@gmail.com',
+                name: 'Medeiros',
+                photo: 'any_photo.png'
+            }
+        });
         const { result } = renderHook(() => useAuth(), {
             wrapper: AuthProvider
         });
@@ -27,5 +24,19 @@ describe('Auth Hook', () => {
         await act(() => result.current.signInWithGoogle());
 
         expect(result.current.user.email).toBe('medeiros@gmail.com');
+    });
+
+    it('user should not connect if cancel authentication with Goolge', async () => {
+        const googleMocked = mocked(logInAsync as any);
+        googleMocked.mockReturnValue({
+            type: 'cancel',
+        });
+        const { result } = renderHook(() => useAuth(), {
+            wrapper: AuthProvider
+        });
+
+        await act(() => result.current.signInWithGoogle());
+
+        expect(result.current.user).not.toHaveProperty('id');
     });
 });
